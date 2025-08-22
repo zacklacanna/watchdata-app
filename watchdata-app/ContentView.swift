6f62208c27da6ec361d29bdb2e0b3a953ec294d8
Zack <lacannazack@gmail.com>
2025-08-21 17:53:14 -0700

    init repo

---
+++ watchdata-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  watchdata-app
+//
+//  Created by Zack Lacanna on 8/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ watchdata-app/watchdata_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchdata_appApp.swift
+//  watchdata-app
+//
+//  Created by Zack Lacanna on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct watchdata_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
