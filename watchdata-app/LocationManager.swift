0f48dd265b7c6a090c4c614c27cfca090ccb0cfd
Zack <lacannazack@gmail.com>
2025-08-21 18:37:07 -0700

    Location Management

---
+++ watchdata-app/LocationManager.swift
@@ -0,0 +1,102 @@
+//
+//  LocationManager.swift
+//  watchdata-app
+//
+//  Created by Zack Lacanna on 8/21/25.
+//
+
+import Foundation
+import CoreLocation
+import Combine
+import UIKit
+
+class LocationManager: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+    
+    @Published var authorizationStatus: CLAuthorizationStatus = .notDetermined
+    @Published var currentLocation: CLLocation?
+    @Published var isLocationEnabled = false
+    
+    override init() {
+        super.init()
+        setupLocationManager()
+    }
+    
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 10 // Update location every 10 meters
+    }
+    
+    func requestLocationPermission() {
+        switch authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .denied, .restricted:
+            // Direct user to Settings
+            if let settingsUrl = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(settingsUrl)
+            }
+        case .authorizedWhenInUse, .authorizedAlways:
+            startLocationUpdates()
+        @unknown default:
+            break
+        }
+    }
+    
+    func startLocationUpdates() {
+        guard authorizationStatus == .authorizedWhenInUse || authorizationStatus == .authorizedAlways else {
+            return
+        }
+        
+        locationManager.startUpdatingLocation()
+        isLocationEnabled = true
+    }
+    
+    func stopLocationUpdates() {
+        locationManager.stopUpdatingLocation()
+        isLocationEnabled = false
+    }
+    
+    func getCurrentLocation() -> CLLocation? {
+        return currentLocation
+    }
+    
+    func calculateDistance(from startLocation: CLLocation, to endLocation: CLLocation) -> CLLocationDistance {
+        return startLocation.distance(from: endLocation)
+    }
+    
+    func calculatePace(distance: CLLocationDistance, time: TimeInterval) -> Double {
+        guard time > 0 else { return 0 }
+        return time / (distance / 1000) // minutes per kilometer
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        currentLocation = location
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location manager failed with error: \(error.localizedDescription)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        DispatchQueue.main.async {
+            self.authorizationStatus = status
+            
+            switch status {
+            case .authorizedWhenInUse, .authorizedAlways:
+                self.startLocationUpdates()
+            case .denied, .restricted:
+                self.stopLocationUpdates()
+            case .notDetermined:
+                break
+            @unknown default:
+                break
+            }
+        }
+    }
+}
